8c8da24cd57f3df4a4443e01180a6e9a896baa25
Jacob Pinchook <jacob.pinchook@gmail.com>
2025-02-02 21:46:09 -0500

    Add calculation functions

---
+++ index.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
+function subtract(a, b) {
+    const subtrahend = a - b;
+    return subtrahend;
+}
+function multiply(a, b) {
+    const product = a * b;
+    return product;
+}
+function divide(a, b) {
+    const remainder = a / b;
+    return remainder;
+}
